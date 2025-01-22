53ebbb84ca6b5c2b6e7601e1346eed3b29b35b6b
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2025-01-22 16:39:24 +0530

    added arrows & youtube video link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,155 @@
+"use client";
 import Link from "next/link"
 import Image from "next/image"
+
+import { useRef } from 'react';
+
 export default function Home() {
+  const SecondPage = useRef<HTMLDivElement | null>(null);
+  const ThirdPage = useRef<HTMLDivElement | null>(null);
+
+  const ScrollToSecondPage = (): void => {
+    if (SecondPage.current) {
+      SecondPage.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const ScrollToThirdPage = (): void => {
+    if (ThirdPage.current) {
+      ThirdPage.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <div>
       {/* First Page */}
-      <div className="h-screen flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center gap-5">
-        <div className="text-center border-b pb-3 border-white/30">
-          <div className="sm:text-8xl text-6xl text-white font-semibold">Welcome!</div>
-          <div className="text-white font-extralight">Effortless Fee Calculations for PayPal & Sofi Wists!</div>
+      <div className="relative h-screen flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-5">
+          <div className="text-center border-b pb-3 border-white/30">
+            <div className="sm:text-8xl text-6xl text-white font-semibold">Welcome!</div>
+            <div className="text-white font-extralight">Effortless Fee Calculations for PayPal & Sofi Wists!</div>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-5 sm:gap-10">
+            <Link
+              href={"/paypal-fee"}
+            >
+              <div className="bg-blue-200 rounded-md px-3 h-[50px] text-blue-950 shadow-blue-600/30 hover:shadow-blue-600/50 shadow-xl hover:scale-110 transition-all ease-in-out duration-500 flex justify-center items-center">
+                <Image
+                  src={"/Paypal.gif"}
+                  height={50}
+                  width={50}
+                  alt="PayPal Gif"
+                />
+                PayPal Fee Calculator
+
+              </div>
+            </Link>
+            <Link
+              href={"/sofi-wist"}
+            >
+              <div className="bg-pink-200 rounded-md px-3 h-[50px] text-pink-950 shadow-pink-600/30 hover:shadow-pink-600/50 shadow-xl hover:scale-110 transition-all ease-in-out duration-500 flex justify-center items-center">
+                <Image
+                  src={"/wist_gif.gif"}
+                  height={40}
+                  width={40}
+                  alt="Sofi Wist Gif"
+                />
+                Sofi Wists Calculator
+              </div>
+            </Link>
+          </div>
         </div>
-        <div className="flex flex-col sm:flex-row gap-5 sm:gap-10">
-          <Link
-            href={"/paypal-fee"}
-          >
-            <div className="bg-blue-200 rounded-md px-3 h-[50px] text-blue-950 shadow-blue-600/30 hover:shadow-blue-600/50 shadow-xl hover:scale-110 transition-all ease-in-out duration-500 flex justify-center items-center">
-            <Image
-            src={"/Paypal.gif"}
+
+        <button onClick={ScrollToSecondPage}>
+          <Image
+            className="absolute left-1/2 bottom-[30px] transform -translate-x-1/2 animate-pulse hover:animate-none hover:scale-110 transition-all duration-300 ease-in-out"
+            src="/down-arrow_white.png"
             height={50}
             width={50}
-            alt="PayPal Gif"
-            />
-            PayPal Fee Calculator
-            
+            alt="arrow"
+          />
+        </button>
+      </div>
+
+      {/* Second Page */}
+      <div ref={SecondPage} className="relative h-screen flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-3">
+          <div className="text-center flex flex-col gap-2">
+            <div className="p-3 text-2xl text-white font-extralight w-[400px] sm:w-[600px] border-b border-white/30">This website is intended solely for educational purposes. I do not support or encourage cross-trading in Sofi, as it is against the rules. Any actions you take within Sofi, including cross-trading or related activities, are your responsibility. I am not accountable for any outcomes or issues that may arise from engaging in this playstyle.</div>
+            <div className="text-white font-extralight">Discord Servers you can join!!</div>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-5 sm:gap-10">
+            <Link
+              href={"https://discord.gg/HgXNs4p5cx"}
+              target="_blank"
+            >
+              <div className="bg-purple-200 rounded-md px-3 h-[50px] text-purple-950 shadow-purple-600/30 hover:shadow-purple-600/50 shadow-xl hover:scale-110 transition-all ease-in-out duration-500 flex justify-start items-center">
+                <Image
+                  src={"/Discord.gif"}
+                  height={50}
+                  width={50}
+                  alt="PayPal Gif"
+                />
+                ItsMe Prince
+
+              </div>
+            </Link>
+            <Link
+              href={"https://discord.gg/sofi"}
+              target="_blank"
+            >
+              <div className="bg-purple-200 rounded-md px-3 h-[50px] text-purple-950 shadow-purple-600/30 hover:shadow-purple-600/50 shadow-xl hover:scale-110 transition-all ease-in-out duration-500 flex justify-start items-center">
+                <Image
+                  src={"/Discord.gif"}
+                  height={50}
+                  width={50}
+                  alt="PayPal Gif"
+                />
+                Sofi Cafe
+              </div>
+            </Link>
+            <Link
+              href={"https://discord.gg/soficorner"}
+              target="_blank"
+            >
+              <div className="bg-purple-200 rounded-md px-3 h-[50px] text-purple-950 shadow-purple-600/30 hover:shadow-purple-600/50 shadow-xl hover:scale-110 transition-all ease-in-out duration-500 flex justify-start items-center">
+                <Image
+                  src={"/Discord.gif"}
+                  height={50}
+                  width={50}
+                  alt="PayPal Gif"
+                />
+                Sofi Corner
+              </div>
+            </Link>
+          </div>
+        </div>
+        <button onClick={ScrollToThirdPage}>
+          <Image
+            className="absolute left-1/2 bottom-[30px] transform -translate-x-1/2 animate-pulse hover:animate-none hover:scale-110 transition-all duration-300 ease-in-out"
+            src="/down-arrow_white.png"
+            height={50}
+            width={50}
+            alt="arrow"
+          />
+        </button>
+      </div>
+      {/* ThirdPage Page */}
+      <div ref={ThirdPage} className="h-screen flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-3">
+          <div className="text-center flex flex-col gap-2">
+            <div className="p-3 text-xl text-white font-extralight w-[400px] sm:w-[600px] border-b border-white/30">You can watch the video in which I&apos;ve explained about this project!</div>
+            <div className="responsive-video">
+              <iframe
+                src="https://www.youtube.com/embed/TmGlgE4iUN4?si=0uv-ugOXFzfnhE7_"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
             </div>
-          </Link>
-          <Link
-            href={"/sofi-wist"}
-          >
-            <div className="bg-pink-200 rounded-md px-3 h-[50px] text-pink-950 shadow-pink-600/30 hover:shadow-pink-600/50 shadow-xl hover:scale-110 transition-all ease-in-out duration-500 flex justify-center items-center">
-            <Image
-            src={"/wist_gif.gif"}
-            height={40}
-            width={40}
-            alt="Sofi Wist Gif"
-            />
-            Sofi Wists Calculator
-            </div>
-          </Link>
+          </div>
         </div>
       </div>
-    </div>
-    {/* Second Page */}
-    <div className="h-screen flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center gap-3">
-        <div className="text-center flex flex-col gap-2">
-          <div className="p-3 text-2xl text-white font-extralight w-[400px] sm:w-[600px] border-b border-white/30">This website is intended solely for educational purposes. I do not support or encourage cross-trading in Sofi, as it is against the rules. Any actions you take within Sofi, including cross-trading or related activities, are your responsibility. I am not accountable for any outcomes or issues that may arise from engaging in this playstyle.</div>
-          <div className="text-white font-extralight">Discord Servers you can join!!</div>
-        </div>
-        <div className="flex flex-col sm:flex-row gap-5 sm:gap-10">
-          <Link
-            href={"https://discord.gg/HgXNs4p5cx"}
-            target="_blank"
-          >
-            <div className="bg-purple-200 rounded-md px-3 h-[50px] text-purple-950 shadow-purple-600/30 hover:shadow-purple-600/50 shadow-xl hover:scale-110 transition-all ease-in-out duration-500 flex justify-start items-center">
-            <Image
-            src={"/Discord.gif"}
-            height={50}
-            width={50}
-            alt="PayPal Gif"
-            />
-            ItsMe Prince
-            
-            </div>
-          </Link>
-          <Link
-            href={"https://discord.gg/sofi"}
-            target="_blank"
-          >
-            <div className="bg-purple-200 rounded-md px-3 h-[50px] text-purple-950 shadow-purple-600/30 hover:shadow-purple-600/50 shadow-xl hover:scale-110 transition-all ease-in-out duration-500 flex justify-start items-center">
-            <Image
-            src={"/Discord.gif"}
-            height={50}
-            width={50}
-            alt="PayPal Gif"
-            />
-            Sofi Cafe
-            </div>
-          </Link>
-          <Link
-            href={"https://discord.gg/soficorner"}
-            target="_blank"
-          >
-            <div className="bg-purple-200 rounded-md px-3 h-[50px] text-purple-950 shadow-purple-600/30 hover:shadow-purple-600/50 shadow-xl hover:scale-110 transition-all ease-in-out duration-500 flex justify-start items-center">
-            <Image
-            src={"/Discord.gif"}
-            height={50}
-            width={50}
-            alt="PayPal Gif"
-            />
-            Sofi Corner
-            </div>
-          </Link>
-        </div>
-      </div>
-    </div>
     </div>
   )
 }
